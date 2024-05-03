a03d03fdf19332c789d85841fe7f550312f74b4b
bolemir1927 <maksim_petrov_88@mail.ru>
2024-05-03 19:05:18 +0300

    0305

--- src/answer2.java
+++ src/answer2.java
@@ -2,9 +2,13 @@ public class answer2 {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        System.out.println("Сумма: " + (a + b)); //Вывод: Сумма:15
-        System.out.println("Разность: " + (a - b));
-        System.out.println("Умножение: " + (a * b));
-        System.out.println("Деление: " + (a / b));
+        int c = a + b;
+        System.out.println("Сумма: " + c); //Вывод: Сумма:15
+        int d = a - b;
+        System.out.println("Разность: " + d);
+        int e = a * b;
+        System.out.println("Умножение: " + e);
+        int f = a / b;
+        System.out.println("Деление: " + f);
     }
 }
--- src/answer5.java
+++ src/answer5.java
@@ -3,11 +3,11 @@ public class answer5 {
         boolean a = false;
         boolean b = true;
         boolean c = false;
-        boolean result1 = (a == a) && b;
+        boolean result1 = !a && b;
         System.out.println("result1 " + result1);
         boolean result2 = a || c;
         System.out.println("result2 " + result2);
-        boolean result3 = b ^ c;
+        boolean result3 = b ^ a;
         System.out.println("result3 " + result3);
 
     }
